c0dcbfa511007e388ed57230627199a3bddd5dc1
seiyamiyaoka <seiyamiyaoka@gmail.com>
2018-09-29 14:12:06 +0900

    100円しか入らない失敗するテストを追加

--- spec/lib/vending_machine_spec.rb
+++ spec/lib/vending_machine_spec.rb
@@ -13,6 +13,22 @@ describe VendingMachine do
       end
     end
   end
+
+  describe '#insert' do
+    context '100を投入する場合' do
+      it 'coinに100が入っていること' do
+        vending_machine.insert(100)
+        expect(vending_machine.coin).to eq(100)
+      end
+    end
+    context '100以外を投入する場合' do
+      it 'coinがnilであること' do
+        vending_machine.insert(500)
+        expect(vending_machine.coin).to eq(nil)
+      end
+    end
+  end
+
   describe 'coin' do
     it 'コインが入る' do
       vending_machine.insert(100)
