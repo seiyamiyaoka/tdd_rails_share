ab9a27e7273a518f9824a1f193384628a7ba7dbd
seiyamiyaoka <seiyamiyaoka@gmail.com>
2018-09-29 13:57:09 +0900

    失敗するテスト作成

--- spec/lib/vending_machine_spec.rb
+++ spec/lib/vending_machine_spec.rb
@@ -2,8 +2,15 @@ describe VendingMachine do
   let(:vending_machine) { VendingMachine.new }
 
   describe 'select' do
-    it 'コーラが出てくる' do
-      expect(vending_machine.select).to eq 'coke'
+    context '100円が入っている場合'
+      it 'コーラが出てくる' do
+        vending_machine.insert(100)
+        expect(vending_machine.select).to eq 'coke'
+      end
+    context '100円が入っていない場合' do
+      it 'コインをいれてねと表示される' do
+        expect(vending_machine.select).to eq 'コインをいれてね'
+      end
     end
   end
   describe 'coin' do
